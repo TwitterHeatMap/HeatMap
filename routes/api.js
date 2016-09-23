dc6f9359610573e7eccd2b5b0d2a3d8bd39fba3a
lisa-seeto <email": null>
2016-09-23 14:59:38 +1200

    intergrated code from sentiment with twitter

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const tweets = require('../lib/getTweetsByLocation')
+const sentiment = require('../lib/getFilteredSentiments')
 
 router.post('/', (req, res, next) => {
 })
@@ -8,9 +9,11 @@ router.get('/', (req, res, next) => {
   var userInput = 'brangelina'
   var geoCodes = '34.0209282,-118.4918063,5km'
   tweets.getTweets(userInput, geoCodes)
-    .then( function (data) {
-      console.log(data)
+    .then(function (data) {
+      console.log(sentiment(data))
+      return sentiment(data)
     })
+
 })
 
 module.exports = router

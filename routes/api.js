02f4077da95e040129c614706e1a54a710cb0145
lisa-seeto <email": null>
2016-09-23 14:25:15 +1200

    get tweets by location hardcode params

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
+const tweets = require('../lib/getTweetsByLocation')
 
 router.post('/', (req, res, next) => {
 })
 
 router.get('/', (req, res, next) => {
-  res.send('Hello world.')
+  var userInput = 'brangelina'
+  var geoCodes = '34.0209282,-118.4918063,5km'
+  tweets.getTweets(userInput, geoCodes)
+    .then( function (data) {
+      console.log(data)
+    })
 })
 
 module.exports = router
